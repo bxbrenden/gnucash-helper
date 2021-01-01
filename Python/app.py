56d8d8070a50005ba564a21fb5e3bdddc60aa784
Brenden Hyde <brendenahyde@gmail.com>
2021-01-01 14:03:57 -0800

    begin dockerizing with new functions

--- Python/app.py
+++ Python/app.py
@@ -5,7 +5,10 @@ from gnucash_helper import list_accounts,\
                            add_transaction,\
                            get_gnucash_dir,\
                            git_pull,\
-                           git_add_commit_and_push
+                           git_add_commit_and_push,\
+                           get_github_token_and_url_from_env,\
+                           git_ensure_cloned,\
+                           git_set_user_and_email
 
 from decimal import Decimal, ROUND_HALF_UP
 from os import environ as env
@@ -51,6 +54,22 @@ app.config['SECRET_KEY'] = env.get('FLASK_SECRET_KEY',
                                    'Mjpe[){i>"r3}]Fm+-{7#,m}qFtf!w)T')
 
 bootstrap = Bootstrap(app)
+
+
+@app.before_first_request
+def configure_git():
+    gnucash_dir = get_gnucash_dir()
+    gh_token, gh_url = get_github_token_and_url_from_env()
+    git_configured = git_set_user_and_email()
+
+    if git_configured:
+        cloned = git_ensure_cloned(gnucash_dir, gh_token, gh_url)
+        if not cloned:
+            print('Git clone of GitHub repo failed. Exiting')
+            sys.exit(1)
+    else:
+        print('git configuration of name and email failed. Exiting')
+        sys.exit(1)
 
 
 @app.route('/', methods=['GET', 'POST'])
