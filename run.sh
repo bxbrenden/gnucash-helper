1193530cf27080445b15c243fa85513bea3a4d3b
Brenden Hyde <brendenahyde@gmail.com>
2021-11-14 10:42:22 -0800

    Update README with steps to run locally, add script for local runs

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pyenv global 3.9.8
+cd ~/git/GnuCash-Helper
+pip install -r requirements.txt
+sudo mkdir /gnucash
+sudo cp ~/git/GnuCash-Helper/demo-budget.gnucash /gnucash
+sudo touch /gnucash-helper.log
+sudo chown -R brenden:brenden /gnucash*
+export GNUCASH_DIR=/gnucash
+export GNUCASH_FILE=demo-budget.gnucash
+export NUM_TRANSACTIONS=200
+gunicorn -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm app:app
