515583c6fdbc4c7584340df5bb46966f3fd2a605
Brenden Hyde <brendenahyde@gmail.com>
2022-08-16 19:10:30 -0700

    Add Pipfiles, modify script to pipenv, dev UX++
    Added Pipfile and Pipfile.lock for local development and pip
    dependency version pinning.
    
    Modify run.sh script to use new pipenv setup and stop messing
    with sudo commands and stuff in the root directory.
    
    These changes improve the experience of developing this tool
    locally, albeit by a small amount.

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-pyenv global 3.9.8
+export GCH_DEV='/home/brenden/gch-dev'
+export GCH_LOG_DIR='/home/brenden/gch-dev/logs'
+pyenv global 3.9.12
 cd ~/git/gnucash-helper
-pip install -r requirements.txt
-sudo mkdir /gnucash
-sudo cp ~/git/gnucash-helper/demo-budget.gnucash /gnucash
-sudo touch /gnucash-helper.log
-sudo chown -R brenden:brenden /gnucash*
-export GNUCASH_DIR=/gnucash
+python3 -m install --upgrade pipenv
+echo -e "Running pipenv install with no args...\n"
+pipenv install
+mkdir -p $GCH_DEV
+mkdir -p $GCH_LOG_DIR
+cp ~/git/gnucash-helper/demo-budget.gnucash $GCH_DEV
+touch $GCH_LOG_DIR/gnucash-helper.log
+export GNUCASH_DIR=$GCH_DEV
 export GNUCASH_FILE=demo-budget.gnucash
 export NUM_TRANSACTIONS=200
-gunicorn -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm app:app
+echo -e "Running gnucash-helper via pipenv run gunicorn...\n"
+pipenv run gunicorn -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm app:app
