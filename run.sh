49971ad585835231b9f32cc931af1529fbaf138c
Brenden Hyde <brendenahyde@gmail.com>
2022-09-12 16:25:47 -0700

    make gunicorn runs work for run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ export FLASK_APP=gch.py
 export GCH_DEV='/home/brenden/gch-dev'
 export GCH_LOG_DIR='/home/brenden/gch-dev/logs'
 export EASY_CONFIG_DIR=$GCH_DEV
-# pyenv global 3.9.12
+pyenv global 3.9.12
 cd ~/git/gnucash-helper
-# python3 -m pip install --upgrade pipenv
-# echo -e "Running pipenv install with no args...\n"
-# pipenv install
+python3 -m pip install --upgrade pipenv
+echo -e "Running pipenv install with no args...\n"
+pipenv install
 mkdir -p $GCH_DEV
 mkdir -p $GCH_LOG_DIR
 if [ ! -f $GCH_DEV/demo-budget.gnucash ]; then
@@ -17,7 +17,9 @@ touch $GCH_LOG_DIR/gnucash-helper.log
 export GNUCASH_DIR=$GCH_DEV
 export GNUCASH_FILE=demo-budget.gnucash
 export NUM_TRANSACTIONS=200
-# echo -e "Running gnucash-helper via pipenv run gunicorn...\n"
-# pipenv run gunicorn -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm app:app
-echo "Running gnucash-helper via 'pipenv run flask run'"
-pipenv run flask run
+echo -e "Running gnucash-helper via pipenv run gunicorn...\n"
+pipenv run gunicorn -b 0.0.0.0:8000 --worker-tmp-dir /dev/shm gch:app
+
+# UNCOMMENT BELOW FOR FLASK RUN
+# echo "Running gnucash-helper via 'pipenv run flask run'"
+# pipenv run flask run
